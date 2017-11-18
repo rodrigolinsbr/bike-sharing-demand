b08bd3e230b06c746b42054f35ccded2199593c0
rodrigolins <rodrigolinsbr@gmail.com>
2017-11-18 11:07:30 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("train.csv")
+#lendo dados
+
+h =df.head(10)
+#mostrando data
+print  df
+d=df.describe()
+#descricao
+
+# name = df['Name'].value_counts()
+# sex = df['Survived'].hist(bins=100)
+# s=df['Survived'].hist(bins=50)
+
+
+#plt.plot(str(sex))
+#plt.title("Muito Fcil")
+#plt.show()
+
+
