e181c98fb591549d422b7e66e4586d5036a3dff1
Johny Moreira <jms5@cin.ufpe.br>
2017-11-27 14:57:13 -0300

    done prediction structure for various models. Missing improve score

--- main.py
+++ main.py
@@ -1,24 +1,127 @@
 import pandas as pd
 import numpy as np
 import calendar
-from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor
-from sklearn.model_selection import StratifiedKFold
+from xgboost import XGBRegressor
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from sklearn import preprocessing, pipeline, linear_model
+from sklearn.model_selection import train_test_split
 
-train = pd.read_csv("train.csv")
-
-train_labels = train.count
-
-del train['registered']
-del train['casual']
-del train['count']
 
 # Extracting new features from datetime in training set
-train.datetime = train.datetime.apply(pd.to_datetime)
-train['day'] = train.datetime.apply(lambda date: date.day)
-train['month'] = train.datetime.apply(lambda date: date.month)
-train['year'] = train.datetime.apply(lambda date: date.year)
-train['weekday'] = train.datetime.apply(lambda date: calendar.weekday(date.year, date.month, date.day))
-train['hour'] = train.datetime.apply(lambda date: date.hour)
+def feature_engineering(dataset):
+    dataset.datetime = dataset.datetime.apply(pd.to_datetime)
+    dataset['day'] = dataset.datetime.apply(lambda date: date.day)
+    dataset['month'] = dataset.datetime.apply(lambda date: date.month)
+    dataset['year'] = dataset.datetime.apply(lambda date: date.year)
+    dataset['weekday'] = dataset.datetime.apply(lambda date: calendar.weekday(date.year, date.month, date.day))
+    dataset['hour'] = dataset.datetime.apply(lambda date: date.hour)
+    del dataset['datetime']
 
-del train['datetime']
+    # delete not used columns
+    if  'count' in dataset:
+        del dataset['count']
+    if 'casual' in dataset:
+        del dataset['casual']
+    if 'registered' in dataset:
+        del dataset['registered']
 
+    return dataset
+
+
+# treats each type of feature (categorical, binary and numerical)
+def transform_feature_type(dataset):
+    binary_columns = ['holiday', 'workingday']
+    binary_indices = np.array([(column in binary_columns) for column in dataset.columns], dtype=bool)
+
+    categorical_columns = ['season', 'weather', 'month', 'year', 'weekday']
+    categorical_indices = np.array([(column in categorical_columns) for column in dataset.columns], dtype=bool)
+
+    numeric_columns = ['temp', 'atemp', 'humidity', 'windspeed', 'hour', 'day']
+    numeric_indices = np.array([(column in numeric_columns) for column in dataset.columns], dtype=bool)
+
+    return binary_indices, categorical_indices, numeric_indices
+
+
+# returns a transformer list with the preprocessing for each type of feature
+# this transformer list will be used by the estimator during training and testing
+def populate_transformer(dataset):
+    binary_indices, categorical_indices, numeric_indices = transform_feature_type(dataset)
+    return [
+        # binary features makes no preprocessing.. only constructs a transformer
+        ('binary_variables_processing', preprocessing.FunctionTransformer(lambda data: data[:, binary_indices])),
+        # continuous data... standard normalization with mean zero
+        ('numeric_variables_processing', pipeline.Pipeline(
+            steps=[('selecting', preprocessing.FunctionTransformer(lambda data: data[:, numeric_indices])),
+                   ('scaling', preprocessing.StandardScaler(with_mean=0))])),
+        # discrete values for categorical data are preprocessed to one hot enconder
+        ('categorical_variables_processing', pipeline.Pipeline(
+            steps=[ ('selecting', preprocessing.FunctionTransformer(lambda data: data[:, categorical_indices])),
+                    ('hot_encoding', preprocessing.OneHotEncoder(handle_unknown='ignore'))
+        ])),
+    ]
+
+
+# Root Mean Squared Logarithmic Error
+def rmsle(y_true, y_pred):
+    log_pred = np.nan_to_num(np.array([np.log(v + 1) for v in y_pred])) # Replace nan with zero and inf with finite numbers.
+    log_true = np.nan_to_num(np.array([np.log(v + 1) for v in y_true])) # Replace nan with zero and inf with finite numbers.
+    calc = (log_pred - log_true) ** 2
+    return np.sqrt(np.mean(calc))
+
+
+x_train = pd.read_csv("train.csv")
+y_train = x_train['count']
+
+x_train = feature_engineering(x_train)
+
+# split data into train and test sets
+test_size = 0.33
+x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=test_size, random_state=42)
+
+# fit model in training data
+#model = XGBRegressor(max_depth=25, learning_rate=0.1, n_estimators=130)
+#model = GradientBoostingRegressor(max_depth = 25, learning_rate=0.1, n_estimators=130)
+model = RandomForestRegressor(max_depth = 25, n_estimators = 130, random_state = 0)
+#model = linear_model.Lasso(max_iter = 2000)
+#model = linear_model.LinearRegression()
+
+# creates a tranformer list
+transformer = populate_transformer(x_train)
+
+# instanciates the estimator using the pipeline for feature processing and the XGBR model
+estimator = pipeline.Pipeline(steps = [
+    ('feature_processing', pipeline.FeatureUnion(transformer_list=transformer)),
+    ('model_fitting', model)
+    ]
+)
+
+# fits training dta
+estimator.fit(x_train, y_train)
+
+# make predictions for test data
+y_pred = estimator.predict(x_test)
+#y_pred = [round(value) for value in y_pred]
+
+# calculates rmlse
+print rmsle(y_test, y_pred)
+
+# makes prediction over the real test dataset from kaggle
+x_real_test = pd.read_csv("test.csv")
+
+# maintain datetime values from test set
+x_real_test_datetime = x_real_test['datetime']
+
+# applies feature engineering to dataset
+x_real_test = feature_engineering(x_real_test)
+
+# makes predictions over real test set
+y_real_test_pred = estimator.predict(x_real_test)
+
+# writes output file for submission
+submission = pd.DataFrame({"datetime": x_real_test_datetime,
+                           "count": [max(0, x) for x in y_real_test_pred]},
+                           columns=['datetime', 'count'])
+
+print submission.head(20)
+
+submission.to_csv('submission.csv', index=False)
